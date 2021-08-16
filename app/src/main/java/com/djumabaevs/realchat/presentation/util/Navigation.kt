161bce61a5cd021d0b32a1148e59445d5f9eeb33
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-16 19:39:38 +0600

    objects different screens in sealed class Screen

--- app/src/main/java/com/djumabaevs/realchat/presentation/util/Navigation.kt
+++ app/src/main/java/com/djumabaevs/realchat/presentation/util/Navigation.kt
@@ -1,2 +1,14 @@
 package com.djumabaevs.realchat.presentation.util
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = ) {
+
+    }
+}
