35d3567f208306646595e04b6606d4153a85d0d6
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-16 17:27:07 +0600

    Initial commit

---
+++ app/src/main/java/com/djumabaevs/realchat/ui/theme/Theme.kt
@@ -0,0 +1,33 @@
+package com.djumabaevs.realchat.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = GreenAccent,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary = DarkGray,
+)
+
+
+
+@Composable
+fun RealChatTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
