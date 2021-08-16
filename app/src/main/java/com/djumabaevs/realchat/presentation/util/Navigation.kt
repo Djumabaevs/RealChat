efffa839bf05a1434d86949213dc85f7a1bdc959
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-16 19:51:04 +0600

    implemented basic navigation setup

--- app/src/main/java/com/djumabaevs/realchat/presentation/util/Navigation.kt
+++ app/src/main/java/com/djumabaevs/realchat/presentation/util/Navigation.kt
@@ -2,13 +2,22 @@ package com.djumabaevs.realchat.presentation.util
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = ) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.SplashScreen.route
+    ) {
+        composable(Screen.SplashScreen.route) {
 
+        }
+        composable(Screen.LoginScreen.route) {
+
+        }
     }
 }
