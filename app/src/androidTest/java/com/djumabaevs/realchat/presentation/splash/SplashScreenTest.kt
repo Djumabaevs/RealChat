67bbf9528f07a47639505c53b70aed4c2175e51d
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-17 12:29:00 +0600

    @RelaxedMockK

--- app/src/androidTest/java/com/djumabaevs/realchat/presentation/splash/SplashScreenTest.kt
+++ app/src/androidTest/java/com/djumabaevs/realchat/presentation/splash/SplashScreenTest.kt
@@ -1,8 +1,10 @@
 package com.djumabaevs.realchat.presentation.splash
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.NavController
 import com.djumabaevs.realchat.presentation.MainActivity
 import com.djumabaevs.realchat.presentation.ui.theme.RealChatTheme
+import io.mockk.impl.annotations.RelaxedMockK
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
@@ -12,11 +14,14 @@ class SplashScreenTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
+    @RelaxedMockK
+    lateinit var navController: NavController
+
     @Test
     fun splashScreen_displaysAndDisappears() {
         composeTestRule.setContent {
             RealChatTheme {
-
+                SplashScreen(navController = navController)
             }
         }
     }
