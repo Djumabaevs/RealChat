1d0712bdd49e1fbb0dc1e1a4de3782c669312ae7
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-17 19:54:50 +0600

    add standard text field to components

---
+++ app/src/main/java/com/djumabaevs/realchat/presentation/components/StandardTextField.kt
@@ -0,0 +1,27 @@
+package com.djumabaevs.realchat.presentation.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun StandardTextField(
+    text: String = "",
+    hint: String = "",
+    onValueChange: (String) -> Unit
+) {
+    TextField(
+        value = text,
+        onValueChange = onValueChange,
+        placeholder = {
+            Text(
+                text = hint,
+                style = MaterialTheme.typography.body1
+            )
+        },
+        modifier = Modifier.fillMaxWidth()
+    )
+}
