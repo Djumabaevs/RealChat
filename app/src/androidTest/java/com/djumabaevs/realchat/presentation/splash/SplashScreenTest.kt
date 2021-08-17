d8ca66574df38f925a45b05b2155c7169f976d65
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-17 12:26:23 +0600

    setContent and RealChat theme

--- app/src/androidTest/java/com/djumabaevs/realchat/presentation/splash/SplashScreenTest.kt
+++ app/src/androidTest/java/com/djumabaevs/realchat/presentation/splash/SplashScreenTest.kt
@@ -1,13 +1,24 @@
 package com.djumabaevs.realchat.presentation.splash
 
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.djumabaevs.realchat.presentation.MainActivity
+import com.djumabaevs.realchat.presentation.ui.theme.RealChatTheme
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 
 class SplashScreenTest {
 
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
     @Test
     fun splashScreen_displaysAndDisappears() {
-        J
+        composeTestRule.setContent {
+            RealChatTheme {
+
+            }
+        }
     }
 
 }
