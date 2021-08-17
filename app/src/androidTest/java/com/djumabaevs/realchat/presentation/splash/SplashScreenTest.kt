ed6f7954e58dd2037421a782348c00594dc11a4d
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-17 12:02:55 +0600

    Junit 4 for compose testing

--- app/src/androidTest/java/com/djumabaevs/realchat/presentation/splash/SplashScreenTest.kt
+++ app/src/androidTest/java/com/djumabaevs/realchat/presentation/splash/SplashScreenTest.kt
@@ -1,7 +1,13 @@
 package com.djumabaevs.realchat.presentation.splash
 
 import org.junit.Assert.*
+import org.junit.Test
 
 class SplashScreenTest {
+
+    @Test
+    fun splashScreen_displaysAndDisappears() {
+        J
+    }
 
 }
