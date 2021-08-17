a30e686b26baeb49fe211709b301b6ffc002dc66
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-17 12:41:56 +0600

    OnNode test content description in runBlockingTest{}

--- app/src/androidTest/java/com/djumabaevs/realchat/presentation/splash/SplashScreenTest.kt
+++ app/src/androidTest/java/com/djumabaevs/realchat/presentation/splash/SplashScreenTest.kt
@@ -1,16 +1,24 @@
 package com.djumabaevs.realchat.presentation.splash
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.navigation.NavController
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.djumabaevs.realchat.presentation.MainActivity
 import com.djumabaevs.realchat.presentation.ui.theme.RealChatTheme
 import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4ClassRunner::class)
 class SplashScreenTest {
 
     @get:Rule
@@ -25,12 +33,14 @@ class SplashScreenTest {
     }
 
     @Test
-    fun splashScreen_displaysAndDisappears() {
+    fun splashScreen_displaysAndDisappears() = runBlockingTest {
         composeTestRule.setContent {
             RealChatTheme {
                 SplashScreen(navController = navController)
             }
         }
+        composeTestRule
+            .onNodeWithContentDescription("Logo")
     }
 
 }
