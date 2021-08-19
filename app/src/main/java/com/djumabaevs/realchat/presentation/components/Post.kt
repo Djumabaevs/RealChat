6d6c5005ab3f81a5907cd9d7915f749e5f590243
Djumabaevs <djumabaevb@gmail.com>
2021-08-19 12:37:22 +0600

    action row composable

--- app/src/main/java/com/djumabaevs/realchat/presentation/components/Post.kt
+++ app/src/main/java/com/djumabaevs/realchat/presentation/components/Post.kt
@@ -1,8 +1,95 @@
 package com.djumabaevs.realchat.presentation.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.djumabaevs.realchat.domain.models.Post
+import com.djumabaevs.realchat.presentation.ui.theme.MediumGray
+import com.djumabaevs.realchat.presentation.ui.theme.ProfilePictureSizeMedium
+import com.djumabaevs.realchat.presentation.ui.theme.SpaceMedium
+import com.djumabaevs.realchat.R
 
 @Composable
 fun Post(
     post: Post,
-)
+    modifier: Modifier = Modifier,
+    showProfileImage: Boolean = true,
+    onPostClick: () -> Unit = {}
+) {
+    Box(modifier = modifier
+        .fillMaxWidth()
+        .padding(SpaceMedium)) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .offset(
+                    y = if (showProfileImage) {
+                        ProfilePictureSizeMedium / 2f
+                    } else 0.dp
+                )
+                .clip(MaterialTheme.shapes.medium)
+                .shadow(5.dp)
+                .background(MediumGray)
+                .clickable {
+                    onPostClick
+                }
+        ) {
+            Image(
+                painterResource(id = R.drawable.kermit),
+                contentDescription = "Post image")
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(SpaceMedium)) {
+                ActionRow()
+
+            }
+        }
+    }
+}
+
+@Composable
+fun ActionRow(
+    modifier: Modifier = Modifier,
+    isLiked: Boolean = false,
+    onLikeClick: (Boolean) -> Unit = {},
+    onCommentClick: () -> Unit = {},
+    onShareClick: () -> Unit = {},
+    username: String,
+    onUsernameClick: (String) -> Unit = {}
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier,
+    ) {
+        Text(
+            text = username,
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colors.primary
+            ),
+            modifier = Modifier
+                .clickable {
+                    onUsernameClick(username)
+                }
+        )
+        EngagementButtons(
+            isLiked = isLiked,
+            onLikeClick = onLikeClick,
+            onCommentClick = onCommentClick,
+            onShareClick = onShareClick
+        )
+    }
+}
