4dff7e210789692feceb86e34f9112bccf778ca8
Djumabaevs <djumabaevb@gmail.com>
2021-08-19 12:18:33 +0600

    bottom nav item data class

--- app/src/main/java/com/djumabaevs/realchat/domain/models/Activity.kt
+++ app/src/main/java/com/djumabaevs/realchat/domain/models/Activity.kt
@@ -1,5 +1,7 @@
 package com.djumabaevs.realchat.domain.models
 
+import com.djumabaevs.realchat.domain.util.ActivityAction
+
 data class Activity(
     val username: String,
     val activityType: ActivityAction,
--- app/src/main/java/com/djumabaevs/realchat/presentation/components/Post.kt
+++ app/src/main/java/com/djumabaevs/realchat/presentation/components/Post.kt
@@ -4,5 +4,5 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun Post(
-post: Post,
+    post: Post,
 )
