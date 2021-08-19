aefa19332b5489560901e7628153b4da2a09a2cc
Djumabaevs <djumabaevb@gmail.com>
2021-08-19 12:38:31 +0600

    engagement buttons composable

--- app/src/main/java/com/djumabaevs/realchat/presentation/components/Post.kt
+++ app/src/main/java/com/djumabaevs/realchat/presentation/components/Post.kt
@@ -4,16 +4,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Comment
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.djumabaevs.realchat.domain.models.Post
 import com.djumabaevs.realchat.presentation.ui.theme.MediumGray
@@ -91,5 +100,66 @@ fun ActionRow(
             onCommentClick = onCommentClick,
             onShareClick = onShareClick
         )
+    }
+}
+
+@Composable
+fun EngagementButtons(
+    modifier: Modifier = Modifier,
+    isLiked: Boolean = false,
+    iconSize: Dp = 30.dp,
+    onLikeClick: (Boolean) -> Unit = {},
+    onCommentClick: () -> Unit = {},
+    onShareClick: () -> Unit = {},
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        IconButton(
+            onClick = {
+                onLikeClick(!isLiked)
+            },
+            modifier = Modifier.size(iconSize)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Favorite,
+                tint = if (isLiked) {
+                    Color.Red
+                } else {
+                    TextWhite
+                },
+                contentDescription = if (isLiked) {
+                    stringResource(id = androidx.compose.foundation.layout.R.string.unlike)
+                } else {
+                    stringResource(id = androidx.compose.foundation.layout.R.string.like)
+                }
+            )
+        }
+        Spacer(modifier = Modifier.width(SpaceMedium))
+        IconButton(
+            onClick = {
+                onCommentClick()
+            },
+            modifier = Modifier.size(iconSize)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Comment,
+                contentDescription = stringResource(id = androidx.compose.foundation.layout.R.string.comment)
+            )
+        }
+        Spacer(modifier = Modifier.width(SpaceMedium))
+        IconButton(
+            onClick = {
+                onShareClick()
+            },
+            modifier = Modifier.size(iconSize)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Share,
+                contentDescription = stringResource(id = androidx.compose.foundation.layout.R.string.share)
+            )
+        }
     }
 }
