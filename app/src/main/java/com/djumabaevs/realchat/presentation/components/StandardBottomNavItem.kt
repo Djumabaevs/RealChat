12c381246c0b83514addbb76c1f7a1dd7985e923
Djumabaevs <djumabaevb@gmail.com>
2021-08-20 10:54:43 +0600

    icon text added

--- app/src/main/java/com/djumabaevs/realchat/presentation/components/StandardBottomNavItem.kt
+++ app/src/main/java/com/djumabaevs/realchat/presentation/components/StandardBottomNavItem.kt
@@ -2,19 +2,28 @@ package com.djumabaevs.realchat.presentation.components
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.djumabaevs.realchat.presentation.ui.theme.HintGray
+import com.djumabaevs.realchat.presentation.ui.theme.SpaceSmall
 
 @Composable
 @Throws(IllegalArgumentException::class)
@@ -69,10 +78,37 @@ fun RowScope.StandardBottomNavItem(
                                 cap = StrokeCap.Round
                             )
                         }
-
-
                     }
-            )
+            ) {
+                if(icon != null) {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = contentDescription,
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                    )
+                }
+                if (alertCount != null) {
+                    val alertText = if (alertCount > 99) {
+                        "99+"
+                    } else {
+                        alertCount.toString()
+                    }
+                    Text(
+                        text = alertText,
+                        color = MaterialTheme.colors.onPrimary,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontSize = 10.sp,
+                        modifier = Modifier
+                            .align(Alignment.TopCenter)
+                            .offset(10.dp)
+                            .size(15.dp)
+                            .clip(CircleShape)
+                            .background(MaterialTheme.colors.primary)
+                    )
+                }
+            }
         }
     )
 
