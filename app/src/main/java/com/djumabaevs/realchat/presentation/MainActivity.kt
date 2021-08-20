86619045a869166b15e45a589b2f05e21576a80b
Djumabaevs <djumabaevb@gmail.com>
2021-08-20 18:27:50 +0600

    main activity refactored

--- app/src/main/java/com/djumabaevs/realchat/presentation/MainActivity.kt
+++ app/src/main/java/com/djumabaevs/realchat/presentation/MainActivity.kt
@@ -6,21 +6,43 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.djumabaevs.realchat.presentation.ui.theme.RealChatTheme
 import com.djumabaevs.realchat.presentation.util.Navigation
+import com.djumabaevs.realchat.presentation.util.Screen
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RealChatTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     color = MaterialTheme.colors.background,
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    Navigation()
+                    val navController = rememberNavController()
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    StandardScaffold(
+                        navController = navController,
+                        showBottomBar = navBackStackEntry?.destination?.route in listOf(
+                            Screen.MainFeedScreen.route,
+                            Screen.ChatScreen.route,
+                            Screen.ActivityScreen.route,
+                            Screen.ProfileScreen.route,
+                        ),
+                        modifier = Modifier.fillMaxSize(),
+                        onFabClick = {
+                            navController.navigate(Screen.CreatePostScreen.route)
+                        }
+                    ) {
+                        Navigation(navController)
+                    }
                 }
             }
         }
