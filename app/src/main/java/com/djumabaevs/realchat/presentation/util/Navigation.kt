54ffa1381a7d5c38a0f8327076d23a28862c8e27
Djumabaevs <djumabaevb@gmail.com>
2021-08-21 10:23:21 +0600

    added screen composables to navigation

--- app/src/main/java/com/djumabaevs/realchat/presentation/util/Navigation.kt
+++ app/src/main/java/com/djumabaevs/realchat/presentation/util/Navigation.kt
@@ -8,7 +8,11 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.djumabaevs.realchat.presentation.activity.ActivityScreen
+import com.djumabaevs.realchat.presentation.chat.ChatScreen
+import com.djumabaevs.realchat.presentation.create_post.CreatePostScreen
 import com.djumabaevs.realchat.presentation.login.LoginScreen
+import com.djumabaevs.realchat.presentation.register.RegisterScreen
 import com.djumabaevs.realchat.presentation.splash.SplashScreen
 
 @Composable
@@ -25,6 +29,41 @@ fun Navigation(navController: NavHostController) {
         }
         composable(Screen.LoginScreen.route) {
             LoginScreen(navController = navController)
+        }
+        composable(Screen.RegisterScreen.route) {
+            RegisterScreen(navController = navController)
+        }
+        composable(Screen.MainFeedScreen.route) {
+            MainFeedScreen(navController = navController)
+        }
+        composable(Screen.ChatScreen.route) {
+            ChatScreen(navController = navController)
+        }
+        composable(Screen.ActivityScreen.route) {
+            ActivityScreen(navController = navController)
+        }
+        composable(Screen.ProfileScreen.route) {
+            ProfileScreen(navController = navController)
+        }
+        composable(Screen.CreatePostScreen.route) {
+            CreatePostScreen(navController = navController)
+        }
+        composable(Screen.PostDetailScreen.route) {
+            PostDetailScreen(
+                navController = navController,
+                post = Post(
+                    username = "Philipp Lackner",
+                    imageUrl = "",
+                    profilePictureUrl = "",
+                    description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed\n" +
+                            "diam nonumy eirmod tempor invidunt ut labore et dolore \n" +
+                            "magna aliquyam erat, sed diam voluptua Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed\\n\" +\n" +
+                            "                    \"diam nonumy eirmod tempor invidunt ut labore et dolore \\n\" +\n" +
+                            "                    \"magna aliquyam erat, sed diam voluptua",
+                    likeCount = 17,
+                    commentCount = 7
+                )
+            )
         }
     }
 }
