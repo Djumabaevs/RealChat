2110d68526f6de7078529db7261b6e27e5d684d3
Djumabaevs <djumabaevb@gmail.com>
2021-08-21 10:38:31 +0600

    finished standard scaffold

--- app/src/main/java/com/djumabaevs/realchat/presentation/components/StandardScaffold.kt
+++ app/src/main/java/com/djumabaevs/realchat/presentation/components/StandardScaffold.kt
@@ -1,15 +1,22 @@
 package com.djumabaevs.realchat.presentation.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Message
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.djumabaevs.realchat.domain.models.BottomNavItem
 import com.djumabaevs.realchat.presentation.util.Screen
+import com.djumabaevs.realchat.R
 
 @Composable
 fun StandardScaffold (
@@ -42,5 +49,50 @@ fun StandardScaffold (
     onFabClick: () -> Unit = {},
     content: @Composable () -> Unit
 ) {
+    Scaffold (
+        bottomBar = {
+            if (showBottomBar) {
+                BottomAppBar(
+                    modifier = Modifier.fillMaxWidth(),
+                    backgroundColor = MaterialTheme.colors.surface,
+                    cutoutShape = CircleShape,
+                    elevation = 5.dp
+                ) {
+                    BottomNavigation {
+                        bottomNavItems.forEachIndexed { i, bottomNavItem ->
+                            StandardBottomNavItem(
+                                icon = bottomNavItem.icon,
+                                contentDescription = bottomNavItem.contentDescription,
+                                selected = bottomNavItem.route == navController.currentDestination?.route,
+                                alertCount = bottomNavItem.alertCount,
+                                enabled = bottomNavItem.icon != null
+                            ) {
+                                if (navController.currentDestination?.route != bottomNavItem.route) {
+                                    navController.navigate(bottomNavItem.route)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        floatingActionButton = {
+            if (showBottomBar) {
+                FloatingActionButton(
+                    backgroundColor = MaterialTheme.colors.primary,
+                    onClick = onFabClick
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = stringResource(id = R.string.make_post)
+                    )
+                }
+            }
+        },
+        isFloatingActionButtonDocked = true,
+        floatingActionButtonPosition = FabPosition.Center,
+        modifier = modifier
+            ) {
 
+    }
 }
