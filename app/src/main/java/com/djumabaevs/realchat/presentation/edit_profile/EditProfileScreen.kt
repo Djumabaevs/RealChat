b82da904412b9d1c3b4b4ee6e737d0bbe1a69d12
Djumabaevs <djumabaevb@gmail.com>
2021-08-29 11:09:44 +0600

    IconButton with onActions

--- app/src/main/java/com/djumabaevs/realchat/presentation/edit_profile/EditProfileScreen.kt
+++ app/src/main/java/com/djumabaevs/realchat/presentation/edit_profile/EditProfileScreen.kt
@@ -2,10 +2,17 @@ package com.djumabaevs.realchat.presentation.edit_profile
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
+import com.djumabaevs.realchat.R
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.djumabaevs.realchat.presentation.components.StandardToolbar
@@ -22,6 +29,15 @@ fun EditProfileScreen(
         StandardToolbar(
             navController = navController,
             showBackArrow = true,
+            navActions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = stringResource(id = R.string.save_changes),
+                        tint = MaterialTheme.colors.onBackground
+                    )
+                }
+            }
         )
     }
 
