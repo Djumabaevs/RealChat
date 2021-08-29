baa57dee48ac0cac29c35dcee5bac91f8caec0aa
Djumabaevs <djumabaevb@gmail.com>
2021-08-29 11:13:10 +0600

    Banner edit section composable

--- app/src/main/java/com/djumabaevs/realchat/presentation/edit_profile/EditProfileScreen.kt
+++ app/src/main/java/com/djumabaevs/realchat/presentation/edit_profile/EditProfileScreen.kt
@@ -1,17 +1,28 @@
 package com.djumabaevs.realchat.presentation.edit_profile
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.djumabaevs.realchat.R
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -30,15 +41,69 @@ fun EditProfileScreen(
             navController = navController,
             showBackArrow = true,
             navActions = {
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = {}) {
                     Icon(
                         imageVector = Icons.Default.Check,
                         contentDescription = stringResource(id = R.string.save_changes),
                         tint = MaterialTheme.colors.onBackground
                     )
                 }
+            },
+            title = {
+                Text(
+                    text = stringResource(id = R.string.edit_your_profile),
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colors.onBackground
+                )
             }
         )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            BannerEd
+
+        }
     }
 
+}
+
+@Composable
+fun BannerEditSection(
+    bannerImage: Painter,
+    profileImage: Painter,
+    profilePictureSize: Dp = ProfilePictureSizeLarge,
+    onBannerClick: () -> Unit = {},
+    onProfileImageClick: () -> Unit = {}
+) {
+    val bannerHeight = (LocalConfiguration.current.screenWidthDp / 2.5f).dp
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(bannerHeight + profilePictureSize / 2f)
+    ) {
+        Image(
+            painter = bannerImage,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(bannerHeight)
+        )
+        Image(
+            painter = profileImage,
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .size(profilePictureSize)
+                .clip(CircleShape)
+                .border(
+                    width = 1.dp,
+                    color = MaterialTheme.colors.onSurface,
+                    shape = CircleShape
+                )
+
+        )
+    }
 }
