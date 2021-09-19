45c73e5ec2435a280a82bd5835791ecfb95013bf
Djumabaevs <djumabaevb@gmail.com>
2021-09-19 10:33:40 +0600

    profile view model

--- app/src/main/java/com/djumabaevs/realchat/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/djumabaevs/realchat/presentation/profile/ProfileViewModel.kt
@@ -1,2 +1,25 @@
 package com.djumabaevs.realchat.presentation.profile
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+
+) : ViewModel() {
+
+    private val _toolbarState = mutableStateOf(ProfileToolbarState())
+    val toolbarState: State<ProfileToolbarState> = _toolbarState
+
+    fun setExpandedRatio(ratio: Float) {
+        _toolbarState.value = _toolbarState.value.copy(expandedRatio = ratio)
+        println("UPDATING TOOLBAR STATE TO $toolbarState")
+    }
+
+    fun setToolbarOffsetY(value: Float) {
+        _toolbarState.value = _toolbarState.value.copy(toolbarOffsetY = value)
+        println("UPDATING TOOLBAR STATE TO $toolbarState")
+    }
+}
