3d245d637f986888cd44ff11698c8204e075b283
Djumabaevs <djumabaevb@gmail.com>
2021-09-25 10:48:49 +0600

    flow of paging data in post repository

--- app/src/main/java/com/djumabaevs/realchat/feature_post/domain/repository/PostRepository.kt
+++ app/src/main/java/com/djumabaevs/realchat/feature_post/domain/repository/PostRepository.kt
@@ -1,10 +1,14 @@
 package com.djumabaevs.realchat.feature_post.domain.repository
 
+import android.net.Uri
 import androidx.paging.PagingData
 import com.djumabaevs.realchat.core.domain.models.Post
+import com.djumabaevs.realchat.core.util.SimpleResource
 import kotlinx.coroutines.flow.Flow
 
 interface PostRepository {
 
     val posts: Flow<PagingData<Post>>
+
+    suspend fun createPost(description: String, imageUri: Uri): SimpleResource
 }
