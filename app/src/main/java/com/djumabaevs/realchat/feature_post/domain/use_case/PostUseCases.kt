575059e3f582809eb27cef552d00a02159a72d6c
Djumabaevs <djumabaevb@gmail.com>
2021-09-25 10:52:19 +0600

    post use cases

--- app/src/main/java/com/djumabaevs/realchat/feature_post/domain/use_case/PostUseCases.kt
+++ app/src/main/java/com/djumabaevs/realchat/feature_post/domain/use_case/PostUseCases.kt
@@ -1,5 +1,6 @@
 package com.djumabaevs.realchat.feature_post.domain.use_case
 
 data class PostUseCases(
-    val getPostsForFollowsUseCase: GetPostsForFollowsUseCase
+    val getPostsForFollowsUseCase: GetPostsForFollowsUseCase,
+    val createPostUseCase: CreatePostUseCase
 )
