cc707e48521400d917fedef91bfed001113dc43a
Djumabaevs <djumabaevb@gmail.com>
2021-09-25 10:48:03 +0600

    post repository impl

--- app/src/main/java/com/djumabaevs/realchat/feature_post/data/repository/PostRepositoryImpl.kt
+++ app/src/main/java/com/djumabaevs/realchat/feature_post/data/repository/PostRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.djumabaevs.realchat.feature_post.data.repository
 
+import android.net.Uri
+import androidx.core.net.toFile
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
@@ -8,14 +10,58 @@ import com.djumabaevs.realchat.core.util.Constants
 import com.djumabaevs.realchat.feature_post.data.data_source.remote.PostApi
 import com.djumabaevs.realchat.feature_post.data.paging.PostSource
 import com.djumabaevs.realchat.feature_post.domain.repository.PostRepository
+import com.google.gson.Gson
 import kotlinx.coroutines.flow.Flow
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import retrofit2.HttpException
+import java.io.IOException
 
 class PostRepositoryImpl(
-    private val api: PostApi
+    private val api: PostApi,
+    private val gson: Gson
 ) : PostRepository {
 
     override val posts: Flow<PagingData<Post>>
         get() = Pager(PagingConfig(pageSize = Constants.PAGE_SIZE_POSTS)) {
             PostSource(api)
         }.flow
+
+    override suspend fun createPost(
+        description: String,
+        imageUri: Uri
+    ): SimpleResource {
+        val request = CreatePostRequest(description)
+        val file = imageUri.toFile()
+        return try {
+            val response = api.createPost(
+                postData = MultipartBody.Part
+                    .createFormData(
+                        "post_data",
+                        gson.toJson(request)
+                    ),
+                postImage = MultipartBody.Part
+                    .createFormData(
+                        name = "post_image",
+                        filename = file.name,
+                        body = file.asRequestBody()
+                    )
+            )
+            if(response.successful) {
+                Resource.Success(Unit)
+            } else {
+                response.message?.let { msg ->
+                    Resource.Error(UiText.DynamicString(msg))
+                } ?: Resource.Error(UiText.StringResource(R.string.error_unknown))
+            }
+        } catch(e: IOException) {
+            Resource.Error(
+                uiText = UiText.StringResource(R.string.error_couldnt_reach_server)
+            )
+        } catch(e: HttpException) {
+            Resource.Error(
+                uiText = UiText.StringResource(R.string.oops_something_went_wrong)
+            )
+        }
+    }
 }
