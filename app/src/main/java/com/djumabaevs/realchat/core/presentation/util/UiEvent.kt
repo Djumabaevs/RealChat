1d22f31bf32abf84d11c277af978382edf420ab5
Djumabaevs <djumabaevb@gmail.com>
2021-09-29 08:24:54 +0600

    ui event sealed class changed

--- app/src/main/java/com/djumabaevs/realchat/core/presentation/util/UiEvent.kt
+++ app/src/main/java/com/djumabaevs/realchat/core/presentation/util/UiEvent.kt
@@ -2,7 +2,9 @@ package com.djumabaevs.realchat.core.presentation.util
 
 import com.djumabaevs.realchat.core.util.UiText
 
+
 sealed class UiEvent {
-    data class SnackbarEvent(val uiText: UiText) : UiEvent()
+    data class ShowSnackbar(val uiText: UiText) : UiEvent()
     data class Navigate(val route: String) : UiEvent()
+    object NavigateUp : UiEvent()
 }
