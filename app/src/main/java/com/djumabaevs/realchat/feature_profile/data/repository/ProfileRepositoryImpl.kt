5750de5f2c27316cdb676b041bd76dfde55c822c
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-05 00:15:40 +0600

    profile repository big refactor

--- app/src/main/java/com/djumabaevs/realchat/feature_profile/data/repository/ProfileRepositoryImpl.kt
+++ app/src/main/java/com/djumabaevs/realchat/feature_profile/data/repository/ProfileRepositoryImpl.kt
@@ -1,16 +1,22 @@
 package com.djumabaevs.realchat.feature_profile.data.repository
 
+import android.net.Uri
+import androidx.core.net.toFile
 import com.djumabaevs.realchat.R
 import com.djumabaevs.realchat.core.util.Resource
 import com.djumabaevs.realchat.core.util.UiText
 import com.djumabaevs.realchat.feature_profile.data.remote.ProfileApi
 import com.djumabaevs.realchat.feature_profile.domain.model.Profile
 import com.djumabaevs.realchat.feature_profile.domain.repository.ProfileRepository
+import com.google.gson.Gson
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
 import retrofit2.HttpException
 import java.io.IOException
 
 class ProfileRepositoryImpl(
-    private val api: ProfileApi
+    private val api: ProfileApi,
+    private val gson: Gson
 ) : ProfileRepository {
 
     override suspend fun getProfile(userId: String): Resource<Profile> {
@@ -23,6 +29,73 @@ class ProfileRepositoryImpl(
                     Resource.Error(UiText.DynamicString(msg))
                 } ?: Resource.Error(UiText.StringResource(R.string.error_unknown))
             }
+        } catch(e: IOException) {
+            Resource.Error(
+                uiText = UiText.StringResource(R.string.error_couldnt_reach_server)
+            )
+        } catch(e: HttpException) {
+            Resource.Error(
+                uiText = UiText.StringResource(R.string.oops_something_went_wrong)
+            )
+        }
+    }
+
+    override suspend fun updateProfile(
+        updateProfileData: UpdateProfileData,
+        bannerImageUri: Uri?,
+        profilePictureUri: Uri?
+    ): SimpleResource {
+        val bannerFile = bannerImageUri?.toFile()
+        val profilePictureFile = profilePictureUri?.toFile()
+
+        return try {
+            val response = api.updateProfile(
+                bannerImage = bannerFile?.let {
+                    MultipartBody.Part
+                        .createFormData(
+                            "banner_image",
+                            bannerFile.name,
+                            bannerFile.asRequestBody()
+                        )
+                },
+                profilePicture = profilePictureFile?.let {
+                    MultipartBody.Part
+                        .createFormData(
+                            "profile_picture",
+                            profilePictureFile.name,
+                            profilePictureFile.asRequestBody()
+                        )
+                },
+                updateProfileData = MultipartBody.Part
+                    .createFormData(
+                        "update_profile_data",
+                        gson.toJson(updateProfileData)
+                    )
+            )
+            if(response.successful) {
+                Resource.Success(Unit)
+            } else {
+                response.message?.let { msg ->
+                    Resource.Error(UiText.DynamicString(msg))
+                } ?: Resource.Error(UiText.StringResource(R.string.error_unknown))
+            }
+        } catch(e: IOException) {
+            Resource.Error(
+                uiText = UiText.StringResource(R.string.error_couldnt_reach_server)
+            )
+        } catch(e: HttpException) {
+            Resource.Error(
+                uiText = UiText.StringResource(R.string.oops_something_went_wrong)
+            )
+        }
+    }
+
+    override suspend fun getSkills(): Resource<List<Skill>> {
+        return try {
+            val response = api.getSkills()
+            Resource.Success(
+                data = response.map { it.toSkill() }
+            )
         } catch(e: IOException) {
             Resource.Error(
                 uiText = UiText.StringResource(R.string.error_couldnt_reach_server)
