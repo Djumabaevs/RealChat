22f30ac5c3b6e1111923ca591fd310a9411bfa51
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-05 00:19:58 +0600

    profile repo refactor

--- app/src/main/java/com/djumabaevs/realchat/feature_profile/domain/repository/ProfileRepository.kt
+++ app/src/main/java/com/djumabaevs/realchat/feature_profile/domain/repository/ProfileRepository.kt
@@ -1,9 +1,21 @@
 package com.djumabaevs.realchat.feature_profile.domain.repository
 
+import android.net.Uri
 import com.djumabaevs.realchat.core.util.Resource
+import com.djumabaevs.realchat.core.util.SimpleResource
 import com.djumabaevs.realchat.feature_profile.domain.model.Profile
+import com.djumabaevs.realchat.feature_profile.domain.model.Skill
+import com.djumabaevs.realchat.feature_profile.domain.model.UpdateProfileData
 
 interface ProfileRepository {
 
     suspend fun getProfile(userId: String): Resource<Profile>
+
+    suspend fun updateProfile(
+        updateProfileData: UpdateProfileData,
+        bannerImageUri: Uri?,
+        profilePictureUri: Uri?
+    ): SimpleResource
+
+    suspend fun getSkills(): Resource<List<Skill>>
 }
